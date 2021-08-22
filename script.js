e91fcc8873af59ed1fbdd9cbdd79ea62b199d01c
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2021-08-22 21:44:09 +0500

    Changing layout and adding copying option

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 var color1=document.querySelector(".color1");
 var color2=document.querySelector(".color2");
-var linerGradient=document.querySelector("h3");
+var linerGradient=document.querySelector("p");
 var body=document.getElementById("gradient")
 
 function addGradient(){
   body.style.background = `linear-gradient(to right,${color1.value},${color2.value})`;
-  console.log(body.style.background)
   linerGradient.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input",addGradient);
 color2.addEventListener("input",addGradient);
+
+function copyFunction() {
+  /* Get the text field */
+  var property = document.getElementById("css-property");
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(property.innerText);
+
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied to clipboard";
+} 
+
